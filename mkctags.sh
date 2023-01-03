87cec2d62fdfb7e8e0675286a001b4bc6defe944
seaeast2 <seaeast2@gmail.com>
2023-01-03 21:24:14 +0900

    extra 옵션이 obsoleted 되어서 extras 라고 변경

--- mkctags.sh
+++ mkctags.sh
@@ -2,6 +2,6 @@
 
 rm -rf tags
 
-ctags --langmap=C++:+.inc+.def,java:+.j+.jj,make:+.mk --c++-kinds=+p --fields=+iaS --extra=+fq --sort=foldcase -R .
+ctags --langmap=C++:+.inc+.def,java:+.j+.jj,make:+.mk --c++-kinds=+p --fields=+iaS --extras=+fq --sort=foldcase -R .
 
 
