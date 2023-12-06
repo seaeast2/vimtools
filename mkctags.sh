c0c58a6fe5af7b1b7521e51ba726e95b636887d2
seaeast2 <hlee2@DESKTOP-I7L4F5R>
2023-12-06 15:10:21 +0900

    update mkctags.sh for yacc support.

--- mkctags.sh
+++ mkctags.sh
@@ -2,6 +2,6 @@
 
 rm -rf tags
 
-ctags --langmap=C++:+.inc+.def,java:+.j+.jj,make:+.mk,yacc:.y --c++-kinds=+p --fields=+iaS --extra=+fq --sort=foldcase -R .
+ctags --langmap=C++:+.inc+.def,Java:+.j+.jj,Make:+.mk,YACC:+.y --c++-kinds=+p --fields=+iaS --extra=+fq --sort=foldcase -R .
 
 
