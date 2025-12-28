2d1dcd8975a7e8ce5ff1b6dffe8fe27d91750670
seaeast2 <seaeast2@gmail.com>
2025-12-28 20:16:41 +0900

    add javascript/typescript language support to mkctags.sh

--- mkctags.sh
+++ mkctags.sh
@@ -2,6 +2,10 @@
 
 rm -rf tags
 
-ctags --langmap=C++:+.inc+.def,Java:+.j+.jj,Make:+.mk,YACC:+.y --c++-kinds=+p --fields=+iaS --extras=+fq --sort=foldcase -R .
-
-
+ctags \
+    --langmap=C++:+.inc+.def,Java:+.j+.jj,Make:+.mk,YACC:+.y,JavaScript:+.js+.jsx,TypeScript:+.ts+.tsx \
+    --c++-kinds=+p \
+    --fields=+iaS \
+    --extras=+fq \
+    --sort=foldcase \
+    -R .
